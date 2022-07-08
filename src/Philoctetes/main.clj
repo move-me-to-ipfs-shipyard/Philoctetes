d9f2fa9ac90ac9ee1f5b6241c99b790f15897df8
Philoctetes <Philoctetes@ooptie-doo>
2022-07-08 06:47:57 +0300

    i revert "i revert "i revert "we run alongside ipfs daemon, i add :ipfs-http-api input field to connect to it"""
    This reverts commit 09e2ca8c57f3bf92184e6fba492b90b384833278.

--- src/Philoctetes/main.clj
+++ src/Philoctetes/main.clj
@@ -401,6 +401,7 @@
     :or {}
     :as opts}]
   (let [jscroll-pane (JScrollPane.)
+
         jcheckbox-apricotseed (JCheckBox.)]
 
     #_(doto jscroll-pane
@@ -410,9 +411,7 @@
     (doto jpanel-tab
       (.setLayout (MigLayout. "insets 10"))
       (.add (JLabel. ":apricotseed?") "cell 0 0")
-      (.add jcheckbox-apricotseed "cell 0 0")
-      (.add (JLabel. ":ipfs-http-api") "cell 0 1")
-      (.add (JTextField. "http://127.0.0.1:5001") "cell 1 1"))
+      (.add jcheckbox-apricotseed "cell 0 0"))
 
     (.addActionListener jcheckbox-apricotseed
                         (reify ActionListener
@@ -420,8 +419,8 @@
                             (SwingUtilities/invokeLater
                              (reify Runnable
                                (run [_]
-                                 #_(put! ops| {:op :settings-value
-                                               :_ (.isSelected jcheckbox-apricotseed)})))))))
+                                 (put! ops| {:op :settings-value
+                                             :apricotseed? (.isSelected jcheckbox-apricotseed)})))))))
 
     (remove-watch settingsA :settings-process)
     (add-watch settingsA :settings-process
@@ -429,7 +428,7 @@
                  (SwingUtilities/invokeLater
                   (reify Runnable
                     (run [_]
-                      (.setSelected jcheckbox-apricotseed (:apricotseed? new-state))))))))
+                      #_(.setSelected jcheckbox-apricotseed (:apricotseed? new-state))))))))
   nil)
 
 (defn -main
